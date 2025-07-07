03487f8c534539458d968cb71b19ad50ede46a27
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-07 16:06:12 +0400

    desktop version

--- src/main/MainContainer.jsx
+++ src/main/MainContainer.jsx
@@ -3,9 +3,30 @@ import Questions from "./questions/QuestionContainer";
 import { useState } from "react";
 export default function Main() {
   return (
-    <div className="container">
-      <div className="img-wrapper"></div>
-      <Questions />
+    <div className="outer-wrapper">
+      <div className="box-wrapper">
+        <img
+          className="box"
+          src="images/illustration-box-desktop.svg"
+          alt="box-img"
+        />
+      </div>
+      <div className="container">
+        <div className="img-wrapper"></div>
+        <div className="desktop-image">
+          <img
+            className="hero"
+            src="images/illustration-woman-online-desktop.svg"
+            alt="hero-img"
+          />
+          <img
+            className="bg"
+            src="images/bg-pattern-desktop.svg"
+            alt="bg-img"
+          />
+        </div>
+        <Questions />
+      </div>
     </div>
   );
 }
